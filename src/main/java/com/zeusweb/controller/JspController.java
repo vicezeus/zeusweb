bb9a0678b0719c0137e2107e7e3959c6a886e335
yeolo03 <yeolo03@naver.com>
2020-06-25 00:28:55 +0900

    travis, appspec set

---
+++ src/main/java/com/zeusweb/controller/JspController.java
@@ -0,0 +1,15 @@
+package com.zeusweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JspController {
+
+    @RequestMapping("/")
+    public String jsp(Model model) throws Exception {
+        model.addAttribute("projectVer", "v1.2");
+        return "main";
+    }
+}
