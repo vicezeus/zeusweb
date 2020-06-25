f971f0a63f7ce89d22fd7121c933381920de2c39
vicezeus <jinsoomail@hanmail.net>
2020-06-25 21:16:57 +0900

    v2

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,14 +1,14 @@
 var main = {
-    init : function () {
-        var _this = this;
-        $('#btn-save').on('click', function () {
-            _this.save();
-        });
-    },
+  init: function () {
+    var _this = this;
+    $("#btn-save").on("click", function () {
+      _this.save();
+    });
+  },
 
-    save : function () {
-        alert("Hello World");
-    }
+  save: function () {
+    alert("Hello World v2");
+  },
 };
 
 main.init();
